a6ae705886fc64e16edaf1cbca5b396d6fd38362
OchoaToo <ochoatoo2009@outlook.com>
2022-10-11 13:01:29 -0500

    Creacion de iconos de de app y Correccion de error de instancia en Botton Iniciar sesion

--- app/src/main/java/com/example/reservas/Recibidor.kt
+++ app/src/main/java/com/example/reservas/Recibidor.kt
@@ -8,7 +8,8 @@ class Recibidor : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btniniciarsesion = findViewById(R.id.btniniciarsesion)
+
+
 
     }
 }
